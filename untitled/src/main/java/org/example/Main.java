ebe24505eff95b55b6f9e7f3c8b73e54e8839f0c
JosephUdeagwu <joearinze100@gmail.com>
2026-09-26 11:31:54 +0100

    Add MongoDB Docker workflow

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,18 +1,29 @@
-// Testing GitHub Actions CI
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        MongoCollection<Document> collection =
+                database.getCollection("test");
+
+        Document doc = new Document("name", "Test Student")
+                .append("class", "Software Engineering")
+                .append("year", "2026")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        collection.insertOne(doc);
+
+        Document myDoc = collection.find().first();
+
+        System.out.println(myDoc.toJson());
     }
 }
